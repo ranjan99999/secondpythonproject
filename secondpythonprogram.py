e92abc0791efe0e5fa13e6db97b5be6f71ea4e21
Ranjan <ranjansingh1114@gmail.com>
2025-03-01 16:28:32 +0530

    added new program of replacing old word to new word

--- secondpythonprogram.py
+++ secondpythonprogram.py
@@ -1,7 +1,7 @@
 # python program to add two value in a key of a dictionary
-# my_dcit = {"a":["ranjan"],"b":["sakshi"]}
-# my_dcit["a"].append("singh")
-# print(my_dcit)
+my_dcit = {"a":["ranjan"],"b":["sakshi"]}
+my_dcit["a"].append("singh")
+print(my_dcit)
 # what if i want add two key in one value
 # no you vcan't do that
 # i am performing pr request and codde will be visible in the pycharm
@@ -17,3 +17,14 @@ for item in my_string.split():
     else:
         empty_string =empty_string +" " + item
 print(empty_string)
+print(my_string)
+
+# printng occurrence of number in the given list
+my_list = [2,3,4,5,3,4,2,7,6,8,9,6,7,4]
+empty_dict = {}
+for obj in my_list:
+    if obj in empty_dict:
+        empty_dict[obj] += 1
+    else:
+        empty_dict[obj] = 1
+print(empty_dict)
